80a802df2ceb821da28b72c88f5a8aa619ff891c
garimachaudhry <35859662+garimachaudhry@users.noreply.github.com>
2018-01-27 10:32:46 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(void) {
+	int n ;
+	scanf("%d",&n);
+	if(n==0)
+  printf("zero");
+  else if(n>0)
+	printf("positive");
+	else 
+  printf("negative");
+	return 0;
+}
